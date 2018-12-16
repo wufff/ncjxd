91d0e2d679135bd7b2b64ec580280c5a485908d0
wufff <wfqjnzb@sina.com>
2018-12-16 16:17:39 +0800

    app.build.js

--- static/app.build.js
+++ static/app.build.js
@@ -5,44 +5,47 @@
    mainConfigFile:"./src/js/re-config.js",
    optimizeCss:"standard",
    removeCombined:true,
+   // modules:[{
+   // 	name:"active_list"
+   // },{
+   // 	name:"active_list_add"
+   // },{
+   //    name:"index"
+   // },{
+   //    name:"issue"
+   // },{
+   //    name:"liveDetail"
+   // },{
+   //    name:"livePay"
+   // },{
+   //    name:"manage_i"
+   // },{
+   //    name:"manage_lower"
+   // },{
+   //    name:"manage_manager"
+   // },{
+   //    name:"manage_n"
+   // },{
+   //    name:"manage_r"
+   // },{
+   //    name:"manage_school"
+   // },{
+   //    name:"manage_t"
+   // },{
+   //    name:"manage_teacher"
+   // },{
+   //    name:"manage_userInfo"
+   // },{
+   //    name:"manage_userPassWrod"
+   // },{
+   //    name:"patrol"
+   // },{
+   //    name:"table"
+   // },{
+   //    name:"table_edit"
+   // }]
    modules:[{
-   	name:"active_list"
-   },{
-   	name:"active_list_add"
-   },{
-      name:"index"
-   },{
-      name:"issue"
-   },{
-      name:"liveDetail"
-   },{
-      name:"livePay"
-   },{
-      name:"manage_i"
-   },{
-      name:"manage_lower"
-   },{
-      name:"manage_manager"
-   },{
-      name:"manage_n"
-   },{
-      name:"manage_r"
-   },{
-      name:"manage_school"
-   },{
-      name:"manage_t"
-   },{
-      name:"manage_teacher"
-   },{
-      name:"manage_userInfo"
-   },{
-      name:"manage_userPassWrod"
-   },{
-      name:"patrol"
-   },{
-      name:"table"
-   },{
-      name:"table_edit"
+        name:"manage_r"
    }]
 })
 
