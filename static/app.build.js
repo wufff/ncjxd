780e55714bf7affbf33ba752425f8e946f6478f5
wufff <wfqjnzb@sina.com>
2018-12-16 16:27:16 +0800

    appbulid.js

--- static/app.build.js
+++ static/app.build.js
@@ -1,16 +1,52 @@
 ({
    appDir:"./src",
    baseUrl:"./js",
-   dir:"./build",
+   dir:"./build1.2",
    mainConfigFile:"./src/js/re-config.js",
    optimizeCss:"standard",
-   removeCombined:true,
+   removeCombined:false,
+   // modules:[{
+   //    name:"active_list"
+   // },{
+   //    name:"active_list_add"
+   // },{
+   //    name:"index"
+   // },{
+   //    name:"issue"
+   // },{
+   //    name:"liveDetail"
+   // },{
+   //    name:"livePay"
+   // },{
+   //    name:"manage_i"
+   // },{
+   //    name:"manage_lower"
+   // },{
+   //    name:"manage_manager"
+   // },{
+   //    name:"manage_n"
+   // },{
+   //    name:"manage_r"
+   // },{
+   //    name:"manage_school"
+   // },{
+   //    name:"manage_t"
+   // },{
+   //    name:"manage_teacher"
+   // },{
+   //    name:"manage_userInfo"
+   // },{
+   //    name:"manage_userPassWrod"
+   // },{
+   //    name:"patrol"
+   // },{
+   //    name:"table"
+   // },{
+   //    name:"table_edit"
+   // }]
    modules:[{
-   	name:"myDialog"
-   },{
-   	name:"myPage"
-   },{
-      name:"index"
+        name:"manage_r",
+        excludeShallow:["layui"]
    }]
 })
 
