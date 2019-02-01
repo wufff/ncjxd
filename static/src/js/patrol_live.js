52e9e85d9b2a782a8585c21e4ab2fe1c10a4df7b
wufff <wfqjnzb@sina.com>
2019-02-01 10:14:32 +0800

    直播详情

--- static/src/js/patrol_live.js
+++ static/src/js/patrol_live.js
@@ -9,17 +9,16 @@
 require(["jquery","layui","path","ckplayer","boot-dropdown"],function($,layui,path,ckplayer){
   var layer = layui.layer;
   var form = layui.form;
-  var obj = [];
+  var arry = [];
  $(".video").each(function(index, el) {
- 	    var id = $(el).attr("id");
- 	    var videoObject = {
-				container: '#'+id,//“#”代表容器的ID，“.”或“”代表容器的class
-				variable: 'player_'+index,//该属性必需设置，值等于下面的new chplayer()的对象
-				flashplayer:false,//如果强制使用flashplayer则设置成true
-				video:"http://video.rjs.dodoedu.com/rjs5b7a853e0ae89.mp4",//视频地址
-				autoplay:true   //是否自动播放
-		  };
-         obj[index] = new ckplayer(videoObject);
+         var file_key = $(this).attr("data-hls");
+                // var file_key = "http://pili-live-hls.dodoedu.com/dodoedu/ncjxd-98ui76yhbgtfdser175m.m3u8?sign=ff0d06746b589e2995d8df906e71720f&t=5c3eaa73";
+                var html =  '<video id="videjs"  class="video-js vjs-big-play-centered vjs-fluid" controls preload="auto" poster=""><source src="'+ file_key +'" type="application/x-mpegURL"></video>'
+                $(this).html(html);
+                arry[index] = videojs('videjs');
+                arry[index].play();
+
    });
-  console.log(obj)
+  console.log(arry)
+     
 })
