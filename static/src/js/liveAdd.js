c2f40a9818be8f2d8c137c65ce08faaa69ae4136
wufff <wfqjnzb@sina.com>
2019-02-21 08:52:16 +0800

    底部样式调整

--- static/src/js/liveAdd.js
+++ static/src/js/liveAdd.js
@@ -1,6 +1,7 @@
-require(["jquery", "layui", "path", "upLoad", "tools","boot-dropdown"], function($, layui, path, upLoad, tools) {
+require(["layui", "path", "upLoad", "tools","api","boot-dropdown"], function(layui, path, upLoad, tools,api) {
   var layer = layui.layer;
   var upload = layui.upload;
+  var $ = jQuery = layui.jquery;
   var form = layui.form;
   var laydate = layui.laydate;
   var upLoad = upLoad.img('upImgBt','previewImage');
@@ -20,7 +21,6 @@ require(["jquery", "layui", "path", "upLoad", "tools","boot-dropdown"], function
   //删除待上传的pm4
 
   $("#goBack").click(function(){
-
       window.history.go(-1);
       return false;
   })
@@ -28,7 +28,6 @@ require(["jquery", "layui", "path", "upLoad", "tools","boot-dropdown"], function
 
 
   //提交
-
   form.on('submit(form)', function(data) {
       var getData = data.field;
       if (getData.cover_img == ""){
@@ -36,7 +35,7 @@ require(["jquery", "layui", "path", "upLoad", "tools","boot-dropdown"], function
          return false;;
       }
       var url = path.api + "/api/addManageActivity";
-      console.log(url)
+      console.log(getData);
       api.ajaxGet(url,getData,function(res){
          if(res.type == "success"){
             layer.msg("操作成功",{time:500});
