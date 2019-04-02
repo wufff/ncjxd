40950d6bc31db574c5b0f4b4e75b171ed85e1fb1
wufff <wfqjnzb@sina.com>
2019-04-02 09:35:13 +0800

    path

--- static/src/js/tools/path.js
+++ static/src/js/tools/path.js
@@ -1,13 +1,13 @@
 define(function(){
-     return 
-     {                                                                   //本地
+     return {  //本地
          img:"http://ncjxd.images.dev.dodoedu.com/static/src/img",
          upLoad:"/upload",
          api:"",
          is_local:false
      }
-     // {                                                                   //线上
-     //     img:"http://ncjxd.images.dev.dodoedu.com/static/src/img",
+     // return { //线上
+     //      img:"http://ncjxd.images.dev.dodoedu.com/static/src/img",
+     //     img:"http://ncjxd.images.boosun.net:8090/src/img",
      //     upLoad:"http://ncjxd.boosun.net:8090/upload",
      //     api:"",
      //     is_local:false 
